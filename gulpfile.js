2a41596e68557fca8489cd606ac25bed74c45701
Felix Xi <felix.xi@sap.com>
2015-12-10 13:00:03 +0800

    Add nw-build to build this project and get executed file will be placed at target folder.

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,17 @@ var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+var NwBuilder = require('nw-builder');
+
 var bases = {
     app: 'build/app/',
-    dist: 'build/'
+    dist: 'build/',
+    build: 'target/'
 };
 
 var paths = {
+    nodemodules: ['node_modules/bcrypt-nodejs/**',
+        'node_modules/diskdb/**'],
     styles: ['bower_components/bootstrap/dist/css/bootstrap.min.css',
         'bower_components/font-awesome/css/font-awesome.min.css',
         'app/assets/css/app.css'
@@ -19,7 +24,8 @@ var paths = {
         'bower_components/bootstrap/dist/fonts/*.*',
         'bower_components/font-awesome/fonts/*.*'
     ],
-    libs: ['bower_components/angular/angular.min.js',
+    libs: ['bower_components/jquery/dist/jquery.min.js',
+        'bower_components/angular/angular.min.js',
         'bower_components/angular-route/angular-route.min.js',
         'bower_components/angular-animate/angular-animate.min.js'
     ],
@@ -30,24 +36,29 @@ var paths = {
 
 // delete the build directory
 gulp.task('clean', function() {
-    return gulp.src(bases.dist)
+    return gulp.src(bases.dist.concat(bases.build))
         .pipe(clean({force: true}));
 });
 
-gulp.task('copy-files', function() {
+gulp.task('copy-files', ['clean'], function() {
     return gulp.src(paths.copyfiles, {base: './'})
         .pipe(gulp.dest(bases.dist));
-})
+});
+
+gulp.task('copy-node-modules', ['clean'], function() {
+    return gulp.src(paths.nodemodules, {base: './'})
+        .pipe(gulp.dest(bases.dist));
+});
 
 // concat vendor js files
-gulp.task('concat-vendor', function() {
+gulp.task('concat-vendor', ['clean'], function() {
     return gulp.src(paths.libs)
         .pipe(concat('vendor.min.js'))
         .pipe(gulp.dest(bases.app + 'js'));
 });
 
 // concat and compress js files
-gulp.task('compressjs', function() {
+gulp.task('compressjs', ['clean'], function() {
     return gulp.src(paths.js)
         .pipe(uglify())
         .pipe(concat('app.min.js'))
@@ -55,19 +66,19 @@ gulp.task('compressjs', function() {
 });
 
 // concat and minify css files
-gulp.task('concat-css', function() {
+gulp.task('concat-css', ['clean'], function() {
     return gulp.src(paths.styles)
         .pipe(minifyCss())
         .pipe(gulp.dest(bases.app + 'css'));
 });
 
-gulp.task('copy-fonts', function() {
+gulp.task('copy-fonts', ['clean'], function() {
     return gulp.src(paths.fonts)
         .pipe(gulp.dest(bases.app + 'fonts'));
 });
 
 // replace css/libs/js with min files
-gulp.task('html-replace', ['copy-files', 'copy-fonts', 'concat-css',
+gulp.task('html-replace', ['clean', 'copy-files', 'copy-fonts', 'concat-css',
     'compressjs', 'concat-vendor'],  function() {
     gulp.src(paths.indexhtml)
         .pipe(htmlreplace({
@@ -78,6 +89,26 @@ gulp.task('html-replace', ['copy-files', 'copy-fonts', 'concat-css',
         .pipe(gulp.dest(bases.dist));
 });
 
+
+// build app 
+gulp.task('build-app', ['clean', 'html-replace', 'copy-node-modules'], function() {
+    // before run this task, need to set proxy if your network is behind firewall,
+    // and nw don't support proxy currently, but you can set the environment varibles
+    // like: HTTP_PROXY=http://proxy.com:port
+    var nw = new NwBuilder({
+        version: '0.12.3',
+        files: './build/**',
+        platforms: ['osx32', 'osx64', 'win32', 'win64'],
+        buildDir: "./target"
+    });
+
+    return nw.build().then(function() {
+        console.log("All Done!");
+    }).catch(function(err) {
+        console.error(err);
+    });
+});
+
 //build full app
-gulp.task('default', ['clean', 'html-replace'],  function() {
+gulp.task('default', ['clean', 'build-app'],  function() {
 });
